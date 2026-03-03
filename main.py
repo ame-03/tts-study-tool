5982d23c4cc1c2e166b7a017bc44f977cce616cc
a3me <ayamentaiko0327@gmail.com>
2026-03-03 18:34:23 +0900

    Fix the audio distortion

--- main.py
+++ main.py
@@ -1,33 +1,48 @@
 from splitter import split_text
 from tts_engine import synthesize
-from player import play_audio
 
 import os
 import subprocess
 
-def play_files(file_list):
-    command = ["mpg123", "-q"] + file_list
-    subprocess.run(command)
+CACHE_DIR = "data/audio_cache"
 
-def play_one(chunks, index):
-    filename = f"output_{index}.mp3"
-    synthesize(chunks[index], filename)
+def ensure_cache_dir():
+    if not os.path.exists(CACHE_DIR):
+        os.makedirs(CACHE_DIR)
+
+def generate_all(chunks):
+    for i, chunk in enumerate(chunks):
+        filename = os.path.join(CACHE_DIR, f"output_{i}.mp3")
+
+        if not os.path.exists(filename):
+            print(f"Generating: {filename}")
+            synthesize(chunk, filename)
+
+
+def play_files(file_list):
+    #command = ["mpg123", "-q"] + file_list
+    #subprocess.call(command)
+    command = "mpg123 -q " + " ".join(file_list)
+    os.system(command)
+
+def play_one(index):
+    filename = os.path.join(CACHE_DIR, f"output_{index}.mp3")
     play_files([filename])
 
-def play_all(chunks):
-    file_list = []
-
-    for i, chunk in enumerate(chunks):
-        filename = f"output_{i}.mp3"
-        synthesize(chunk, filename)
-        file_list.append(filename)
-
+def play_all(num_chunks):
+    file_list = [
+        os.path.join(CACHE_DIR, f"output_{i}.mp3")
+        for i in range(num_chunks)
+    ]
     play_files(file_list)
 
 def main():
     text = "And this heat, this energy, it's always on, it's clean, and it actually holds our whole world together."
 
     chunks = split_text(text, max_words=15)
+
+    ensure_cache_dir()
+    generate_all(chunks)
 
     for i, chunk in enumerate(chunks):
         print(f"[{i}] {chunk}")
@@ -38,13 +53,13 @@ def main():
     choice = input("Select: ")
 
     if choice == "a":
-        play_all(chunks)
+        play_all(len(chunks))
     elif choice == "q":
         return
     elif choice.isdigit():
         index = int(choice)
         if 0 <= index < len(chunks):
-            play_one(chunks, index)
+            play_one(index)
 
 if __name__ == "__main__":
     main()
