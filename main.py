05f16e7d274c8301221b1ae614f699d68b949633
a3me <ayamentaiko0327@gmail.com>
2026-03-03 17:51:03 +0900

    Select and play audio on CLI

--- main.py
+++ main.py
@@ -2,17 +2,49 @@ from splitter import split_text
 from tts_engine import synthesize
 from player import play_audio
 
+import os
+import subprocess
+
+def play_files(file_list):
+    command = ["mpg123", "-q"] + file_list
+    subprocess.run(command)
+
+def play_one(chunks, index):
+    filename = f"output_{index}.mp3"
+    synthesize(chunks[index], filename)
+    play_files([filename])
+
+def play_all(chunks):
+    file_list = []
+
+    for i, chunk in enumerate(chunks):
+        filename = f"output_{i}.mp3"
+        synthesize(chunk, filename)
+        file_list.append(filename)
+
+    play_files(file_list)
+
 def main():
     text = "And this heat, this energy, it's always on, it's clean, and it actually holds our whole world together."
 
     chunks = split_text(text, max_words=15)
 
-    first_chunk = chunks[0]
+    for i, chunk in enumerate(chunks):
+        print(f"[{i}] {chunk}")
 
-    filename = "output.mp3"
+    print("[a] play all")
+    print("[q] quit")
 
-    synthesize(first_chunk, filename)
-    play_audio(filename)
+    choice = input("Select: ")
+
+    if choice == "a":
+        play_all(chunks)
+    elif choice == "q":
+        return
+    elif choice.isdigit():
+        index = int(choice)
+        if 0 <= index < len(chunks):
+            play_one(chunks, index)
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 import os
 
 def play_audio(filename):
-    os.system(f"mpg123 {filename}")
+   os.system(f"mpg123 {filename}")
+    #os.system(f"cmd.exe /c start {filename}")
